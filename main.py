6c07a8b912524cf9668e8591abc8be481e2ec102
Yaroslav Ivanov <jaroslawrussia@gmail.com>
2026-05-09 06:51:21 +0500

    feat: add async and logs

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from aiogram import Bot, Dispatcher, F, types
+from aiogram import Bot,Dispatcher,F,types
 from aiogram.filters import Command
 import asyncio
 from dotenv import load_dotenv
 import os
-from openai import OpenAI
+import logging
+from openai import AsyncOpenAI
 
-load_dotenv()
+load_dotenv()   
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
 api_key = os.getenv("YANDEX_CLOUD_API_KEY")
 folder = os.getenv("YANDEX_CLOUD_FOLDER")
 model = os.getenv("YANDEX_CLOUD_MODEL")
 bot_token = os.getenv("BOT_TOKEN")
-bot_username = os.getenv("BOT_USERNAME")
-system_prompt = os.getenv("SYSTEM_PROMPT")
+bot_name = os.getenv("BOT_NAME")
 
-MAX_HISTORY = 20
+with open("promts/system.txt", "r", encoding="utf-8") as file:
+    system_prompt = file.read()
+
+MAX_HISTORY = 20     
+KEEP_RECENT = 10     
 SUMMARY_LENGTH = 100
 
-client = OpenAI(
-    api_key=api_key, base_url="https://ai.api.cloud.yandex.net/v1", project=folder
+client = AsyncOpenAI(
+  api_key=api_key,
+  base_url="https://ai.api.cloud.yandex.net/v1",
+  project=folder
 )
 
 history = {
@@ -25,7 +34,7 @@ history = {
 }
 
 
-bot = Bot(token=bot_token)
+bot = Bot(token = bot_token)
 dp = Dispatcher()
 
 
@@ -34,59 +43,58 @@ async def handle_mentions(message: types.Message):
 
     if not message.text:
         return
-
+    
     text = message.text
-    print(text)
 
     chat_id = message.chat.id
-    user_name = message.from_user.username
+    user_name =  message.from_user.username
 
-    history.setdefault(chat_id, []).append(
-        {"role": "user", "content": f"{user_name}:{text}"}
-    )
+    history.setdefault(chat_id, []).append({"role": "user", "content": f"{user_name}:{text}"})
 
-    history[chat_id] = history[chat_id][-MAX_HISTORY:]
-
-    bot_mentioned = f"@{bot_username}" in text
+    bot_mentioned =  f"@{bot_name}" in text
 
     if bot_mentioned:
-        response_text = generate_ai_response(chat_id)
-        history[chat_id].append({"role": "assistant", "content": response_text})
-        await message.reply(response_text)
-
-    summarize_history(chat_id)
+        try:
+            response_text = await generate_ai_response(chat_id)
+            history[chat_id].append({"role": "assistant", "content": response_text})
+            await message.reply(response_text)
+        except Exception as e:
+            logger.error(f"Error generating AI response: {e}")
+            await message.reply("Произошла ошибка при генерации ответа. Попробуйте позже.")
+            return
+    
+    await summarize_history(chat_id)
 
     return
 
 
-def generate_ai_response(chat_id):
+async def generate_ai_response(chat_id):
 
-    messages = [{"role": "system", "content": system_prompt}, *history[chat_id]]
+    messages = [{"role": "system", "content": system_prompt},
+                *history[chat_id]]
 
-    completion = client.chat.completions.create(
-        model=f"gpt://{folder}/{model}", messages=messages
+    completion = await client.chat.completions.create(
+    model=f"gpt://{folder}/{model}",
+    messages=messages
     )
-
+    
     response_text = completion.choices[0].message.content
     return response_text
 
 
-def summarize_history(chat_id):
-    if len(history[chat_id]) < MAX_HISTORY:
+async def summarize_history(chat_id):
+    if len(history[chat_id]) < MAX_HISTORY:  
         return
-    old_messages = history[chat_id][:-10]
+    old_messages = history[chat_id][:-KEEP_RECENT]  
     if not old_messages:
         return
-    prompt = f"""Суммаризируй эту беседу. Оставь важные детали. 
-    Размер саммари ограничено {SUMMARY_LENGTH} словами.\n""" + "\n".join(
-        [f"{m['role']}: {m['content']}" for m in old_messages]
-    )
-    completion = client.chat.completions.create(
-        model=f"gpt://{folder}/{model}", messages=[{"role": "user", "content": prompt}]
+    prompt = system_prompt + "\n".join([f"{m['role']}: {m['content']}" for m in old_messages])
+    completion = await client.chat.completions.create(
+        model=f"gpt://{folder}/{model}",
+        messages=[{"role": "user", "content": prompt}]
     )
     summary = completion.choices[0].message.content
-    history[chat_id] = [{"role": "system", "content": summary}] + history[chat_id][-10:]
-
+    history[chat_id] = [{"role": "system", "content": summary}] + history[chat_id][-KEEP_RECENT:]      
 
 async def main():
     await dp.start_polling(bot)
